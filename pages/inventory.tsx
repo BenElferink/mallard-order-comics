e2e8b35285bd15148fb6e6a2ebc92cbaf7d7e0a9
Ben Elferink <ben.elferink@icloud.com>
2023-09-18 12:24:08 +0300

    UI tweak

--- pages/inventory.tsx
+++ pages/inventory.tsx
@@ -12,19 +12,19 @@ const Page = () => {
   const { populatingWallet, populatedWallet } = useAuth()
 
   return (
-    <main className='min-h-screen px-12 flex flex-col items-center'>
-      <h2 className={`text-center text-4xl sm:text-6xl font-normal ${imbue.className}`}>MY INVENTORY</h2>
+    <main className='min-h-screen mb-12 px-12 flex flex-col items-center'>
+      <h2 className={`mb-8 text-center text-4xl sm:text-6xl font-normal ${imbue.className}`}>MY INVENTORY</h2>
 
       {!populatedWallet?.stakeKey ? (
         <div className='mt-8'>{populatingWallet ? <Loader /> : <Auth />}</div>
       ) : (
         <div className='w-full flex flex-col items-center'>
-          <div className={`w-full my-4 p-8 flex items-center justify-center rounded-lg bg-red-950/80 text-xl sm:text-3xl ${antonio.className}`}>
+          <div className={`w-full p-8 mb-8 flex items-center justify-center rounded-lg bg-red-950/80 text-xl sm:text-3xl ${antonio.className}`}>
             TOTAL COLLECTOR&apos;S POINTS:{' '}
             <span className='w-2/5 p-4 ml-8 text-center rounded-lg border border-red-800 bg-red-950'>{populatedWallet.points} POINTS</span>
           </div>
 
-          <div className='w-full mb-12 max-h-[150vh] overflow-y-scroll p-8 flex flex-wrap justify-center rounded-lg bg-neutral-900/50'>
+          <div className='w-full max-h-[150vh] overflow-y-scroll p-8 flex flex-wrap justify-center rounded-lg bg-neutral-900/50'>
             {populatedWallet.tokens.map((token) => {
               const displayName = token.name.split('-')[1].toUpperCase().trim()
               const coverRarity = token.coverVariant.toLowerCase().replaceAll(' ', '_')
