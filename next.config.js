5e093f02e9e223859d10923c0ef6edbaa7f86249
Ben Elferink <ben.elferink@icloud.com>
2023-09-19 18:55:51 +0300

    api cors edits

--- next.config.js
+++ next.config.js
@@ -11,6 +11,18 @@ const nextConfig = {
     }
     return config
   },
+  async headers() {
+    return [
+      {
+        source: '/api/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'OPTIONS,GET,POST' },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
